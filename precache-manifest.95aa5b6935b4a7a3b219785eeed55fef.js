c4399a7f822e822c31f6817a7e0c183f311dd37b
Igor Shapoval <123sgor.shapoval@gmail.com>
2021-09-01 09:25:41 +0300

    Updates

---
+++ precache-manifest.95aa5b6935b4a7a3b219785eeed55fef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fbbb1f731c37ce8c94764b8e0e453dd",
+    "url": "./index.html"
+  },
+  {
+    "revision": "af2b20c57ec75df9c7f2",
+    "url": "./static/css/main.ae4f840b.chunk.css"
+  },
+  {
+    "revision": "bef14ca31350064182c6",
+    "url": "./static/js/2.e8d9cca5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.e8d9cca5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af2b20c57ec75df9c7f2",
+    "url": "./static/js/main.3e33917b.chunk.js"
+  },
+  {
+    "revision": "e447b2ddaa3314191b11",
+    "url": "./static/js/runtime-main.e90a3edd.js"
+  }
+]);
